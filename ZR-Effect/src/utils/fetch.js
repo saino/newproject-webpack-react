8087b926c227dfee87c834ea15890a64398a8fb9
xxoo <zwb461153861@sina.cn>
2018-03-19 19:35:20 +0800

    前端ajax处理，集中处理http异常，数据错误

---
+++ ZR-Effect/src/utils/fetch.js
@@ -0,0 +1,59 @@
+// import config from '../config';
+
+function checkErrorCodeStatus (resp: Object) {
+  if (resp.errorCode == 0)
+    return resp;
+
+   throw new Error(resp.errorMessage);
+}
+
+function parseResp (resp: Object) {
+  return resp.json();
+}
+
+function parseQs (qs) {
+  let res = '?', key;
+
+  for (key in qs) {
+    res += `${ encodeURIComponent(key) }=${ encodeURIComponent(qs[ key ]) }&`
+  }
+
+  return res;
+}
+
+const fetchConfig = {
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json; charset=UTF-8'
+  }
+};
+const request = (path: String, method: String, body: Object) => {
+  const isPost = method.toLowerCase() === 'post';
+  const token = body.token;
+  fetchConfig.headers.Token = token;
+  delete body.token;
+
+  return fetch(
+    //`${ config.api.host }${ path }${ isPost ? '' : parseQs(body) }`,
+    `${ path }${ isPost ? '' : parseQs(body) }`,
+    {...fetchConfig, method, body: isPost ? JSON.stringify(body) : void 0 }
+  )
+  .then(parseResp)
+  .then(checkErrorCodeStatus);
+};
+
+export function get (path: String, qs = {}, success: Function, fail: Function) {
+  return request(path, 'GET', qs)
+   .then(resp => success(resp.data))
+   .catch(error => {
+     fail && fail(error.message);
+   });
+};
+
+export function post (path: String, body = {}, success: Function, fail: Function) {
+  return request(path, 'POST', body)
+   .then(resp => success(resp.data))
+   .catch(error => {
+     fail && fail(error.message);
+   });
+};
