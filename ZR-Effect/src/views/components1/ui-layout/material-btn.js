c19f8370af725730a47eed129638b0702e6dd16b
liubiao <liubiao@qq.com>
2018-05-16 18:56:05 +0800

    素材层级调换

--- ZR-Effect/src/views/components1/ui-layout/material-btn.js
+++ ZR-Effect/src/views/components1/ui-layout/material-btn.js
@@ -10,18 +10,83 @@ import DeleImg from "../../statics/dele.png";
 
 class MaterialBtn extends Component {
     render(){
-        const icon =  this.props.model.type == "audio" ? AudioImg : VideoImg;
-        const materialClass = ClassNames({"material-btn": true, "active": this.props.model.active});
-        const lineClass = ClassNames({"bottom-line": true, "active": this.props.model.active});
-        return <div className={materialClass} onClick={this.onMaterialClick}>
-            <div className="thum-warp">
-                <div className="thum-icon"><img src={icon} /> </div>
-                {this.props.model.name}
-            </div>
+        // console.log(this, "kkkkkkkkkkkkk");
+        const { dragHandle } = this.props;
+        const icon =  this.props.item.type == "audio" ? AudioImg : VideoImg;
+        const materialClass = ClassNames({"material-btn": true, "active": this.props.item.active});
+        const lineClass = ClassNames({"bottom-line": true, "active": this.props.item.active});
+        // return <div className={materialClass} onClick={this.onMaterialClick}>
+        //     {dragHandle(<div>
+        //         <div className="thum-warp">
+        //             <div className="thum-icon"><img src={icon} /> </div>
+        //             {this.props.item.name}
+        //         </div>
+        //         {
+        //             this.props.item.active ? <div onClick={this.onDeleMaterialClick}><img src={DeleImg} /></div> : null
+        //         }
+        //         < div className = { lineClass } />
+        //     </div>)}
+        //     {/* <div className="thum-warp">
+        //         <div className="thum-icon"><img src={icon} /> </div>
+        //         {this.props.item.name}
+        //     </div>
+        //     {
+        //         this.props.item.active ? <div onClick={this.onDeleMaterialClick}><img src={DeleImg}/></div> : null
+        //     }
+        //     <div className={lineClass}/> */}
+        //     <style>{`
+        //         .material-btn{
+        //             height: 40px;
+        //             width: 160px;
+        //             padding: 0 16px;
+        //             font-size: 14px;
+        //             line-height: 40px;
+        //             display: flex;
+        //             cursor: pointer;
+        //             justify-content: space-between;
+        //             color: #fff;
+        //             position: relative;
+        //         }
+        //         .bottom-line{
+        //             position: absolute;
+        //             width: 100%;
+        //             height: 1px;
+        //             background: #0D1D21;
+        //             left: 0;
+        //             bottom: 0;
+        //         }
+        //         .bottom-line.active{
+        //              background-image: linear-gradient(-269deg, rgba(146,202,204,0.40) 0%, rgba(109,170,173,0.99) 32%, #6DABAE 64%, rgba(88,153,157,0.40) 100%);
+        //         }
+        //         .active{
+        //             background: #0D1D21;
+        //         }
+        //         .thum-warp{
+        //             display: flex;
+        //             overflow: hidden;
+        //             white-space: nowrap;
+        //             text-overflow: ellipsis;
+        //         }
+        //         .thum-icon{
+        //             height: 12px;
+        //             width: 12px;
+        //             margin-right: 8px;
+        //         }
+        //     `}</style>
+        // </div>
+        return <div>
             {
-                this.props.model.active ? <div onClick={this.onDeleMaterialClick}><img src={DeleImg}/></div> : null
+                dragHandle(<div className={materialClass} onClick={this.onMaterialClick}>
+                    <div className="thum-warp">
+                        <div className="thum-icon"><img src={icon} /> </div>
+                        {this.props.item.name}
+                    </div>
+                    {
+                        this.props.item.active ? <div onClick={this.onDeleMaterialClick}><img src={DeleImg} /></div> : null
+                    }
+                    < div className={lineClass} />
+                </div>)
             }
-            <div className={lineClass}/>
             <style>{`
                 .material-btn{
                     height: 40px;
@@ -44,7 +109,7 @@ class MaterialBtn extends Component {
                     bottom: 0;
                 }
                 .bottom-line.active{
-                     background-image: linear-gradient(-269deg, rgba(146,202,204,0.40) 0%, rgba(109,170,173,0.99) 32%, #6DABAE 64%, rgba(88,153,157,0.40) 100%);
+                        background-image: linear-gradient(-269deg, rgba(146,202,204,0.40) 0%, rgba(109,170,173,0.99) 32%, #6DABAE 64%, rgba(88,153,157,0.40) 100%);
                 }
                 .active{
                     background: #0D1D21;
@@ -61,14 +126,15 @@ class MaterialBtn extends Component {
                     margin-right: 8px;
                 }
             `}</style>
-        </div>
+        </div>;
     }
     onDeleMaterialClick = (e) => {
         e.stopPropagation();
         e.preventDefault();
-        const {work, model} = this.props;
+        const {work, item} = this.props;
+        // const { item } =
         let materials = work.config.materials.reduce((materials, materialItem, index)=>{
-            if(materialItem.id!==model.id){
+            if(materialItem.id!==item.id){
                 materials.push(materialItem);
             }
             return materials
@@ -76,10 +142,10 @@ class MaterialBtn extends Component {
         this.props.changeWorkMaterial(materials);
     }
     onMaterialClick = () => {
-        const {work, model} = this.props;
+        const {work, item} = this.props;
         let materials = work.config.materials.map((materialItem, index)=>{
             materialItem.active = false;
-            if(materialItem.id===model.id){
+            if(materialItem.id===item.id){
                 materialItem.active = true;
             }
             return materialItem;
